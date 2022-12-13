f82438d339b2c5d5870833d4fd2fc43fdeda0b93
kirubel <kirubelsolomons280@gmail.com>
2022-12-13 12:03:49 -0800

    task 7

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 /**
  * main - check the code
  * Return: Always 0.
@@ -9,6 +10,9 @@ int r;
 print_last_digit(98);
 print_last_digit(0);
 r = print_last_digit(-1024);
+_putchar('0' + r);
+_putchar('\n');
+r = print_last_digit(INT_MIN);
 _putchar('0' + r);
 _putchar('\n');
 return (0);
