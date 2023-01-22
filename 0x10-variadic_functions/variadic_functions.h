7ca3cb8399c67b0e459bde3ef137e8f7254c61ec
Kira solo <kirubelsolomons280@gmail.com>
2023-01-22 00:06:21 -0800

    task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,5 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
-#include <stdarg.h>
-#include <stdio.h>
+#ifndef _VARIADIC_H_
+#define _VARIADIC_H_
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
