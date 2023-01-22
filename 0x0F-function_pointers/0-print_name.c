4f01791046746ba93a106f36598828e0e9600473
Kira solo <kirubelsolomons280@gmail.com>
2023-01-22 12:30:56 -0800

    task 3

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include "function_pointers.h"
 /**
  * print_name - prints a name
  * @name: name to print
@@ -7,7 +5,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-if (!name || !f)
-return;
+if (name && f)
 f(name);
 }
--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -2,7 +2,6 @@
 #define CALC_H
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
  * struct op - Struct op
  * @op: The operator
@@ -19,4 +18,4 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-#endif
+#endif /*CALC_H*/
